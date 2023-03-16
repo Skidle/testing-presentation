5bf57a3220cc44f35615d877bed0002f49d22654
Daria Doronina <skidle.dd@gmail.com>
2023-03-16 13:25:07 +0100

    fixes

--- src/App.js
+++ src/App.js
@@ -1,41 +1,55 @@
+import { useEffect, useState } from 'react';
 import { Greeting } from './components/example_basic';
 import { userService } from './components/userService';
 import { UserProfileEdit } from './components/example_advanced';
 
-const App = () => (
-    <>
-        <header>
-            <nav>
-                <ul>
-                    <li><a href="#example_1">Příklad 1</a></li>
-                    <li><a href="#example_2">Příklad 2</a></li>
-                </ul>
-            </nav>
-            <h1>Praktické testování Reactu</h1>
-            <p>pomocí Jest a React testing library</p>
-        </header>
-        <main>
-            <hr />
-            <section>
-                <header>
-                    <h2 id="example_1">Příklad 1</h2>
-                </header>
-                <aside>
-                    <Greeting name='Jane' />
-                </aside>
-            </section>
-            <hr />
-            <section>
-                <header>
-                    <h2 id="example_2">Příklad 2</h2>
-                </header>
-                <UserProfileEdit onSave={() => { }} user={userService.getUser()} />
-            </section>
-        </main>
-        <footer>
-            <hr />
-        </footer>
-    </>
-)
+const App = () => {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        async function fetchUser() {
+            const data = await userService.getUser();
+            setUser(data);
+        }
+        fetchUser();
+    }, []);
+
+
+    return (
+        <>
+            <header>
+                <nav>
+                    <ul>
+                        <li><a href="#example_1">Příklad 1</a></li>
+                        <li><a href="#example_2">Příklad 2</a></li>
+                    </ul>
+                </nav>
+                <h1>Praktické testování Reactu</h1>
+                <p>pomocí Jest a React testing library</p>
+            </header>
+            <main>
+                <hr />
+                <section>
+                    <header>
+                        <h2 id="example_1">Příklad 1</h2>
+                    </header>
+                    <aside>
+                        <Greeting name='Jane' />
+                    </aside>
+                </section>
+                <hr />
+                <section>
+                    <header>
+                        <h2 id="example_2">Příklad 2</h2>
+                    </header>
+                    {user ? <UserProfileEdit onSave={(data) => console.log(data)} user={user} /> : <p>Loading ...</p>}
+                </section>
+            </main>
+            <footer>
+                <hr />
+            </footer>
+        </>
+    )
+}
 
 export default App;
--- src/__tests__/example_advanced.test.js
+++ src/__tests__/example_advanced.test.js
@@ -5,23 +5,24 @@ import { UserProfileEdit } from '../components/example_advanced';
 // Popisuje testování komponenty UserProfileEdit
 describe('UserProfileEdit', () => {
     // Arrange: nastavíme test a potřebná data
-    test('should submit form data correctly', async () => {
-        // Mockování API volání
-        const submitFormMock = jest.fn().mockResolvedValue({});
 
-        // Testovací data
-        const user = {
-            name: 'John Doe',
-            email: 'john.doe@example.com',
-            password: 'password',
-        };
+    // Mockování API volání
+    const submitFormMock = jest.fn().mockResolvedValue({});
 
-        // Vykreslení komponenty
+    // Testovací data
+    const user = {
+        name: 'John Doe',
+        email: 'john.doe@example.com',
+        password: 'password',
+    };
+
+    test('should submit form data correctly', () => {
+        // Act: zobrazíme komponentu a vyplníme pole formuláře
+
         render(
             <UserProfileEdit onSave={submitFormMock} user={user} />
         );
 
-        // Act: vyplníme pole formuláře
         fireEvent.change(screen.getByLabelText('Name'), { target: { value: 'Jane Doe' } });
         fireEvent.change(screen.getByLabelText('Email'), { target: { value: 'jane.doe@example.com' } });
         fireEvent.change(screen.getByLabelText('Password'), { target: { value: 'newpassword' } });
